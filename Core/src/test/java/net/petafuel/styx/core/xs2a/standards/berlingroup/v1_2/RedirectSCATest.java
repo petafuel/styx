a07f70dcb69ae9061986ebf7f10018c1d1bbc6ad
pF-Luis <52947253+pF-luis@users.noreply.github.com>
2019-09-11 15:24:57 +0200

    Tools-8998 BasicService + BerlinGroupSigner Refactoring für Client Zertifikat

---
+++ Core/src/test/java/net/petafuel/styx/core/xs2a/standards/berlingroup/v1_2/RedirectSCATest.java
@@ -0,0 +1,52 @@
+package net.petafuel.styx.core.xs2a.standards.berlingroup.v1_2;
+
+import net.petafuel.styx.core.banklookup.XS2AStandard;
+import net.petafuel.styx.core.xs2a.entities.Account;
+import net.petafuel.styx.core.xs2a.entities.Consent;
+import net.petafuel.styx.core.xs2a.entities.PSU;
+import net.petafuel.styx.core.xs2a.exceptions.BankRequestFailedException;
+import net.petafuel.styx.core.xs2a.standards.berlingroup.v1_2.http.CreateConsentRequest;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.security.SignatureException;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RedirectSCATest {
+
+    @Test
+    public void startSCA() throws SignatureException, BankRequestFailedException {
+        XS2AStandard standard = new XS2AStandard();
+        standard.setCs(new BerlinGroupCS("https://xs2a-sandbox.f-i-apim.de:8444/fixs2a-env/xs2a-api/12345678"));
+
+        Assert.assertTrue(standard.isCSImplemented());
+
+        List<Account> balances = new LinkedList<>();
+        balances.add(new Account("DE40100100103307118608"));
+        balances.add(new Account("DE02100100109307118603"));
+        balances.add(new Account("DE67100100101306118605"));
+
+        List<Account> transactions = new LinkedList<>();
+        transactions.add(new Account("DE40100100103307118608"));
+
+
+        PSU psu = new PSU("4321-87654321-4321");
+
+        // build Request Body
+        CreateConsentRequest createConsentRequest = new CreateConsentRequest();
+        createConsentRequest.getAccess().setBalances(balances);
+        createConsentRequest.getAccess().setTransactions(transactions);
+        createConsentRequest.setPsu(psu);
+        createConsentRequest.setCombinedServiceIndicator(false);
+        createConsentRequest.setRecurringIndicator(false);
+        createConsentRequest.setFrequencyPerDay(4);
+        createConsentRequest.setValidUntil(new Date());
+
+        Consent consent = standard.getCs().createConsent(createConsentRequest);
+
+        //TODO call sca link
+        //return redirect link to client
+    }
+}

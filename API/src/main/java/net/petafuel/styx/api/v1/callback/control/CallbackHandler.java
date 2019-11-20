8d599c1caa1b6d6ca86d77a76ef6eae528cf288b
pF-ThomasM <pF-ThomasM@users.noreply.github.com>
2019-11-20 13:33:39 +0100

    Tools-9207 Redirect Landing Page implementieren

--- API/src/main/java/net/petafuel/styx/api/v1/callback/control/CallbackHandler.java
+++ API/src/main/java/net/petafuel/styx/api/v1/callback/control/CallbackHandler.java
@@ -6,26 +6,37 @@ import net.petafuel.styx.core.xs2a.oauth.entities.OAuthSession;
 import net.petafuel.styx.core.xs2a.oauth.http.TokenRequest;
 
 import net.petafuel.styx.core.xs2a.utils.Config;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-import java.net.URI;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public class CallbackHandler {
 
     private static final Logger LOG = LogManager.getLogger(CallbackHandler.class);
     public Response handleRedirect(String xRequestId, HttpHeaders httpHeaders, String body) {
-
         System.out.println("Handle Request " + xRequestId);
         StringBuilder output = new StringBuilder();
         for(String field : httpHeaders.getRequestHeaders().keySet()){
             output.append(" ").append(field).append(": ").append(httpHeaders.getRequestHeader(field)).append("\n");
         }
         LOG.info("\n Request Header \n" + output + "\n Request Body \n" + body);
-        String message = "Gettings Callbacks with x-request-id: " + xRequestId;
-        return Response.status(200).entity(message).build();
+
+        try (InputStream input = new FileInputStream("API/src/main/resources/index.html")) {
+            Optional<String> o1 = Optional.of(IOUtils.toString(input, StandardCharsets.UTF_8.toString()));
+            // TODO linkToRedirect
+            String linkToRedirect = "https://google.de/";
+            String htmlContent = o1.get().replace("scaLink",  linkToRedirect);
+            return Response.status(Response.Status.TEMPORARY_REDIRECT).entity(htmlContent).build();
+        } catch (Exception e) {
+            return Response.status(200).entity("hello world").build();
+        }
     }
 
     public Response handleOAuth2(String code, String state, String error, String errorMessage) {
@@ -37,8 +48,10 @@ public class CallbackHandler {
             linkToRedirect = handleFailedOAuth2(state);
         }
 
-        try {
-            return Response.temporaryRedirect(new URI(linkToRedirect)).build();
+        try (InputStream input = new FileInputStream("API/src/main/resources/index.html")) {
+            Optional<String> o1 = Optional.of(IOUtils.toString(input, StandardCharsets.UTF_8.toString()));
+            String htmlContent = o1.get().replace("scaLink",  linkToRedirect);
+            return Response.status(Response.Status.TEMPORARY_REDIRECT).entity(htmlContent).build();
         } catch (Exception e) {
             String message = "Handling callback with code: " + code + " and state: " + state;
             return Response.status(200).entity(message).build();

6f378e7c7e58fd93524af5c2441846656566660a
pF-Luis <52947253+pF-luis@users.noreply.github.com>
2019-12-09 09:05:07 +0100

    Tools-9259 config changes

--- Core/src/main/java/net/petafuel/styx/core/persistence/Persistence.java
+++ Core/src/main/java/net/petafuel/styx/core/persistence/Persistence.java
@@ -1,6 +1,7 @@
 package net.petafuel.styx.core.persistence;
 
 import net.petafuel.dbutils.DbHelper;
+import net.petafuel.styx.core.xs2a.utils.Config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,26 +13,22 @@ import java.sql.SQLException;
  * General Database Connection Class
  *
  * <p>
- *     Singleton, only one connection to the database is allowed<br>
- *     <code>Connection connection = Persistence.getInstance().getConnection();</code>
+ * Singleton, only one connection to the database is allowed<br>
+ * <code>Connection connection = Persistence.getInstance().getConnection();</code>
  * </p>
+ *
  * @since 1.0-SNAPSHOT
  */
 public class Persistence {
     private static final Logger LOG = LogManager.getLogger(Persistence.class);
 
-    private static final String DATASOURCE_NAME = "prep_styx";
     private static Persistence singletonInstance;
 
     private Connection databaseConnection;
 
-    private static class Holder {
-        private static final Persistence INSTANCE = new Persistence();
-    }
-
     private Persistence() {
         try {
-            this.databaseConnection = new DbHelper().getConnection(DATASOURCE_NAME);
+            this.databaseConnection = new DbHelper().getConnection(Config.getInstance().getProperties().getProperty("persistence.dbname"));
         } catch (SQLException | NamingException e) {
             LOG.error("Error connecting to the database: {} \n {}", e.getMessage(), e.getCause());
             throw new PersistenceException(e.getMessage(), e);
@@ -44,5 +41,9 @@ public class Persistence {
 
     public Connection getConnection() {
         return this.databaseConnection;
+    }
+
+    private static class Holder {
+        private static final Persistence INSTANCE = new Persistence();
     }
 }

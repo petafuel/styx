4dae43274120d85f6b779d3bc47bc79daab32f4f
pF-mentor <53050344+pF-mentor@users.noreply.github.com>
2020-02-21 10:49:38 +0100

    Tools-9451 GET /v1/payments/{paymentProduct}/{paymentId}/status fertigstellen

--- API/src/main/java/net/petafuel/styx/api/WebServer.java
+++ API/src/main/java/net/petafuel/styx/api/WebServer.java
@@ -24,7 +24,6 @@ import net.petafuel.styx.api.v1.consent.boundary.ConsentResource;
 import net.petafuel.styx.api.v1.payment.boundary.FetchPaymentResource;
 import net.petafuel.styx.api.v1.payment.boundary.PaymentInitiationResource;
 import net.petafuel.styx.api.v1.payment.boundary.PaymentStatusResource;
-import net.petafuel.styx.api.v1.payment.boundary.PaymentStatusResource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Request;

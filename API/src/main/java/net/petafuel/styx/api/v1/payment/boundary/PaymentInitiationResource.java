d4f62fa479d8e0986007e0766b4c37287deb4cea
pF-david <53561289+pF-david@users.noreply.github.com>
2020-04-02 10:31:48 +0200

    Tools-9618 POST /v1/consents/{consentId}/authorisations

--- API/src/main/java/net/petafuel/styx/api/v1/payment/boundary/PaymentInitiationResource.java
+++ API/src/main/java/net/petafuel/styx/api/v1/payment/boundary/PaymentInitiationResource.java
@@ -33,8 +33,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.UUID;

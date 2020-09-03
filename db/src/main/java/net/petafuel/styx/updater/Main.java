5c914dde9911dd968d4cc5cf174adf8a7b446086
pF-luis <52947253+pf-luis@users.noreply.github.com>
2020-09-03 11:58:14 +0200

    * liquibase.example corrected syntax from yaml to java properties * Main.java explicitly close Database connection using the AutoClosable Interface by try-with-resource

--- db/src/main/java/net/petafuel/styx/updater/Main.java
+++ db/src/main/java/net/petafuel/styx/updater/Main.java
@@ -19,55 +19,50 @@ import java.sql.DriverManager;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            if (isOperationRequested(args, "command=dump"))
-            {
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        try {
+            if (isOperationRequested(args, "command=dump")) {
                 String currentTimeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 
-                String params[] = {
+                String[] params = {
                         "--changeLogFile=dump-scheme-" + currentTimeStamp + ".xml",
                         "--logLevel=info",
                         "generateChangeLog",
                 };
 
                 liquibase.integration.commandline.Main.main(params);
-            }
-            else
-            {
+            } else {
                 setLogLevelInfo();
-                Database database = openDbConnection();
-                String changeLogFile = selectLiquibaseFile(args);
-                Liquibase liquibase = new Liquibase(changeLogFile, new ClassLoaderResourceAccessor(), database);
-                liquibase.update(new Contexts(), new LabelExpression());
-                System.out.println("All database changes run successfully");
+                try (Connection connection = openDbConnection()) {
+                    Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+                    String changeLogFile = selectLiquibaseFile(args);
+                    Liquibase liquibase = new Liquibase(changeLogFile, new ClassLoaderResourceAccessor(), database);
+                    liquibase.update(new Contexts(), new LabelExpression());
+                    logger.info("All database changes run successfully");
+                }
             }
-        } catch (Exception e)
-        {
-            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
         }
     }
 
     /**
      * Selects liquibase entry point based on supplied command parameter
+     *
      * @param args
      * @return
      */
-    protected static String selectLiquibaseFile(String ... args)
-    {
-        for(String argument : args) {
-            if(argument.startsWith("command")) {
-                switch(argument) {
-                    case "command=init":
-                        return "liquibase/master-init.xml";
-                    case "command=update":
-                        return "liquibase/master-update.xml";
-                }
+    protected static String selectLiquibaseFile(String... args) {
+        for (String argument : args) {
+            if (argument.equals("command=init")) {
+                return "liquibase/master-init.xml";
+            } else if (argument.equals("command=update")) {
+                return "liquibase/master-update.xml";
             }
         }
         throw new IllegalArgumentException("Unrecognized parameter provided. One of following supported: command=dump, command=init, command=update");
@@ -76,59 +71,46 @@ public class Main
     /**
      * Sets loglevel to INFO to display excecuted SQL queries
      */
-    private static void setLogLevelInfo()
-    {
+    private static void setLogLevelInfo() {
         ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
         rootLogger.setLevel(Level.INFO);
     }
 
     /**
      * Establishes database connection
+     *
      * @return Instance of JdbcConnection
      * @throws Exception if config files contents invalid credentials
      */
-    private static Database openDbConnection() throws Exception
-    {
-        System.out.println(new File(".").getAbsoluteFile());
-        try (InputStream input = new FileInputStream("liquibase.properties"))
-        {
+    private static Connection openDbConnection() {
+        try (InputStream input = new FileInputStream("liquibase.properties")) {
             // load a properties file
             Properties config = new Properties();
             config.load(input);
-            Class<?> driver = Class.forName(config.getProperty("driver"));
-            Connection connection = DriverManager.getConnection(
+            return DriverManager.getConnection(
                     config.getProperty("url"),
                     config.getProperty("username"),
                     config.getProperty("password"));
-            return DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
-        }
-        catch (FileNotFoundException ex)
-        {
+        } catch (FileNotFoundException ex) {
             throw new IllegalArgumentException("Configuration liquibase.properties file not found", ex);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             throw new IllegalArgumentException("Database connection failed", ex);
         }
-
     }
 
     /**
      * Verifies whether operation parameter were provided
+     *
      * @param arguments list of parameters programm started with
      * @param operation paramenter to be checked
      * @return true if desired parameter was provided, otherwise false
      */
-    protected static boolean isOperationRequested(String[] arguments, String operation)
-    {
-        if (arguments.length == 0)
-        {
+    protected static boolean isOperationRequested(String[] arguments, String operation) {
+        if (arguments.length == 0) {
             return false;
         }
-        for (String argument : arguments)
-        {
-            if (operation.equalsIgnoreCase(argument))
-            {
+        for (String argument : arguments) {
+            if (operation.equalsIgnoreCase(argument)) {
                 checkConfigExists();
                 return true;
             }
@@ -138,13 +120,12 @@ public class Main
 
     /**
      * Verfies whether properties file is available
+     *
      * @throws IllegalArgumentException if config file is not present
      */
-    private static void checkConfigExists()
-    {
+    private static void checkConfigExists() {
         File path = new File(".");
-        if (!new File("liquibase.properties").exists())
-        {
+        if (!new File("liquibase.properties").exists()) {
             throw new IllegalArgumentException("Please ensure the config file liquibase.properties exists in the folder " + path.getAbsoluteFile());
         }
     }

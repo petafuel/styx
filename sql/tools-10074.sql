5638bcdb1dceeffe2f4fc97fc25254e8a7cc2501
pF-luis <52947253+pf-luis@users.noreply.github.com>
2020-12-23 10:52:20 +0100

    * new config for sparda, only allow oauth

--- sql/tools-10074.sql
+++ sql/tools-10074.sql
@@ -1,30 +1,405 @@
---
---Adjust get_bpd_data to only use active banks
---
-DROP FUNCTION get_bpd_data(bics text[]);
-CREATE OR REPLACE FUNCTION get_bpd_data(bics text[])
-    RETURNS TABLE
-            (
-                bic              text,
-                sca_methods      json,
-                prestep_required boolean
-            )
-    SECURITY DEFINER
-    LANGUAGE sql
-as
-$$
-SELECT aspsps.bic,
-       COALESCE(
-               configs.config -> 'IO5' -> 'options',
-               standards.config_template -> 'IO5' -> 'options'
-           )                AS sca_methods,
-       COALESCE(
-               configs.config -> 'IO6' -> 'options' -> 'required',
-               standards.config_template -> 'IO6' -> 'options' -> 'required'
-           )::text::boolean AS prestep_required
-FROM configs
-         INNER JOIN standards ON standards.id = configs.standard_id
-         INNER JOIN aspsps ON aspsps.config_id = configs.id
-WHERE aspsps.bic = ANY ($1)
-  AND aspsps.active = TRUE;
-$$;
+--only allow oauth sca approach for sparda banks
+UPDATE configs
+SET config = '{
+  "IO1": {
+    "options": {
+      "required": true
+    },
+    "description": "Mandate the TPP to sign requests on application level"
+  },
+  "IO2": {
+    "options": {
+      "sepa-credit-transfers": true,
+      "instant-sepa-credit-transfers": false,
+      "target-2-payments": false,
+      "cross-border-credit-transfers": false,
+      "pain.001-sepa-credit-transfers": false,
+      "pain.001-instant-sepa-credit-transfers": false,
+      "pain.001-target-2-payments": false,
+      "pain.001-cross-border-credit-transfers": false
+    },
+    "description": "Support single payment products"
+  },
+  "IO3": {
+    "options": {
+      "sepa-credit-transfers": false,
+      "instant-sepa-credit-transfers": false,
+      "target-2-payments": false,
+      "cross-border-credit-transfers": false,
+      "pain.001-sepa-credit-transfers": false,
+      "pain.001-instant-sepa-credit-transfers": false,
+      "pain.001-target-2-payments": false,
+      "pain.001-cross-border-credit-transfers": false
+    },
+    "description": "Support bulk payment products"
+  },
+  "IO4": {
+    "options": {
+      "sepa-credit-transfers": true,
+      "instant-sepa-credit-transfers": false,
+      "target-2-payments": false,
+      "cross-border-credit-transfers": false,
+      "pain.001-sepa-credit-transfers": false,
+      "pain.001-instant-sepa-credit-transfers": false,
+      "pain.001-target-2-payments": false,
+      "pain.001-cross-border-credit-transfers": false
+    },
+    "description": "Support periodic payment products"
+  },
+  "IO5": {
+    "options": {
+      "redirect": false,
+      "oauth": true,
+      "decoupled": false,
+      "embedded": false
+    },
+    "description": "Support SCA approaches"
+  },
+  "IO6": {
+    "options": {
+      "required": true
+    },
+    "description": "OAuth2 required as a pre-step for PSU authentication (of the first factor)"
+  },
+  "IO9": {
+    "options": {
+      "psu_authentication_required": true,
+      "psu_identification_required": true,
+      "authentication_method_selection_required": true,
+      "no_requirements": true
+    },
+    "description": "Risk management regarding the offering of SCA methods via the XS2A interface"
+  },
+  "IO10": {
+    "options": {
+      "available": true
+    },
+    "description": "Transaction fees transported via the XS2A interface"
+  },
+  "IO11": {
+    "options": {
+      "sms_otp": true,
+      "chip_otp": true,
+      "photo_otp": true,
+      "push_otp": true
+    },
+    "description": "Supported SCA Methods"
+  },
+  "IO12": {
+    "options": {
+      "available": true
+    },
+    "description": "Configuration of supported SCA methods - applicable SCA approaches"
+  },
+  "IO13": {
+    "options": {
+      "available": true
+    },
+    "description": "Configuration of supported SCA methods - TPP redirect preferred"
+  },
+  "IO14": {
+    "options": {
+      "identification_sufficient": true,
+      "authentication_required": true
+    },
+    "description": "Authentication requirements for the decoupled SCA approach"
+  },
+  "IO15": {
+    "options": {
+      "payment_initiation_request": true,
+      "account_information_consent_request": true,
+      "payment_cancellation": true,
+      "signing_basket": true
+    },
+    "description": "PSU-ID required in message"
+  },
+  "IO16": {
+    "options": {
+      "payment_initiation_request": true,
+      "account_information_consent_request": true,
+      "payment_cancellation": true,
+      "signing_basket": true
+    },
+    "description": "PSU-ID-Type required in message"
+  },
+  "IO17": {
+    "options": {
+      "available": true
+    },
+    "description": "Support of multicurrency accounts"
+  },
+  "IO18": {
+    "options": {
+      "iban": true,
+      "bban": false,
+      "pan": false,
+      "masked_pan": false,
+      "msisdn": false
+    },
+    "description": "Representation of an account"
+  },
+  "IO19": {
+    "options": {
+      "payment_initiation_request": true,
+      "account_information_consent_request": true,
+      "payment_cancellation": true,
+      "signing_basket": true
+    },
+    "description": "PSU-Corporate-ID required in message, if a corporate account is affected"
+  },
+  "IO20": {
+    "options": {
+      "payment_initiation_request": true,
+      "account_information_consent_request": true,
+      "payment_cancellation": true,
+      "signing_basket": true
+    },
+    "description": "PSU-Corporate-ID-type required in message, if a corporate account is affected"
+  },
+  "IO21": {
+    "options": {
+      "available": true
+    },
+    "description": "Support of future dated payments"
+  },
+  "IO22": {
+    "options": {
+      "if_creditor_account_belongs_to_PSU": true,
+      "if_creditor_is_on_a_whitelist_of_the_PSU": true,
+      "if_instructed_amount_does_not_exceed_a_certain_limit": true,
+      "transaction_risk_analyse": true
+    },
+    "description": "Support of SCA exemption"
+  },
+  "IO23": {
+    "options": {
+      "available": true
+    },
+    "description": "Support of sessions (combination of AIS and PIS)"
+  },
+  "IO24": {
+    "options": {
+      "decoupled_sca_initiated": true,
+      "sca_method_chosen_embedded": true
+    },
+    "description": "Support of PSU messages in relevant scenarios"
+  },
+  "IO25": {
+    "options": {
+      "signing_baskets_for_the_same_payment_product_allowed_only_individual_payments": true,
+      "signing_baskets_for_the_various_payment_products_allowed_only_individual_payments": true,
+      "signing_baskets_for_the_same_payment_product_allowed_also_payments_with_multi_level_sca": true,
+      "signing_baskets_for_the_various_payment_products_allowed_also_payments_with_multi_level_sca": true,
+      "signing_baskets_for_payments_and_consent_establishment_allowed_only_individual_payments": true,
+      "signing_baskets_for_payments_and_consent_establishment_allowed_also_payments_with_multi_level_sca": true
+    },
+    "description": "Grouping restrictions for signing baskets"
+  },
+  "IO26": {
+    "options": {
+      "required": true
+    },
+    "description": "SCA required for payment cancellation"
+  },
+  "IO27": {
+    "options": {
+      "payment_initiation_request": true,
+      "account_information_consent_request": true,
+      "payment_cancellation": true,
+      "signing_basket": true
+    },
+    "description": "Multi level SCA supported for use cases"
+  },
+  "IO28": {
+    "options": {
+      "redirect": true,
+      "embedded": true,
+      "decoupled": true
+    },
+    "description": "Multi level SCA supported for use cases"
+  },
+  "IO29": {
+    "options": {
+      "redirect": true,
+      "embedded": true,
+      "decoupled": true
+    },
+    "description": "ASPSP enforces explicit start of authorisation"
+  },
+  "IO30": {
+    "options": {
+      "all_psd2_related_services_for_all_accounts": true,
+      "only_access_right_in_request,_accounts_handled_between_psu_and_aspsp_afterwards": true,
+      "list_of_available_accounts": true,
+      "list_of_available_accounts_with_balances": true
+    },
+    "description": "Support of optional account Information access rights"
+  },
+  "IO31": {
+    "options": {
+      "camt.052": true,
+      "camt.053": true,
+      "camt.054": true,
+      "json": true,
+      "mt942": true,
+      "mt940": true
+    },
+    "description": "Support of formats for account information"
+  },
+  "IO32": {
+    "options": {
+      "accounts_with_balance": true,
+      "accounts_account-id_with_balance": true,
+      "accounts_account-id_transactions_with_balance": true,
+      "accounts_account-id_transactions_resourceId": true
+    },
+    "description": "Support of optional Endpoints for AIS"
+  },
+  "IO33": {
+    "options": {
+      "entry_reference_from": true,
+      "booking_status_pending": true,
+      "booking_status_both": true,
+      "delta_list": true
+    },
+    "description": "Support of optional (values of) query parameters for AIS"
+  },
+  "IO34": {
+    "options": {
+      "opening_booked052": true,
+      "expected": true,
+      "interim_available": true,
+      "forward_available": true,
+      "non_invoiced": true,
+      "closing_booked": true
+    },
+    "description": "Support of balance types"
+  },
+  "IO35": {
+    "options": {
+      "always": true,
+      "never": true,
+      "if_the_size_of_the_transaction_list_does_not_exceed_1_gb": true
+    },
+    "description": "Conditions for delivery of a transaction list directly in the response"
+  },
+  "IO36": {
+    "options": {
+      "always": true,
+      "never": true,
+      "if_the_size_of_the_transaction_list_does_exceed_1_gb": true
+    },
+    "description": "Conditions for delivery of a transaction list as a separate download with only a link in the response"
+  },
+  "IO37": {
+    "options": {
+      "available": true
+    },
+    "description": "Redirect after first SCA-Factor"
+  },
+  "IO38": {
+    "options": {
+      "available": true
+    },
+    "description": "Implicit start of transaction authorisation supported"
+  },
+  "IO39": {
+    "options": {
+      "available": true
+    },
+    "description": "API steering links of type ''''startAuthorisationWith...'''' supported (i.e. creation of authorisation sub-resources and delivery of missing data at the same time supported)"
+  },
+  "IO40": {
+    "options": {
+      "required": true
+    },
+    "description": "PSU authentication data delivered via the XS2A-interface (embedded approach) shall be encrypted at application level"
+  },
+  "IO41": {
+    "options": {
+      "aggregation_level": true,
+      "sub-account_level": true
+    },
+    "description": "Access to multicurrency account details"
+  },
+  "IO42": {
+    "options": {
+      "available": true
+    },
+    "description": "Card number supported to identify subaccounts"
+  },
+  "IO43": {
+    "options": {
+      "payments_sepa-credit-transfers": true,
+      "payments_instant-sepa-credit-transfers": true,
+      "payments_target-2-payments": true,
+      "payments_cross-border-credit-transfers": true,
+      "payments_pain.001-sepa-credit-transfers": true,
+      "payments_pain.001-instant-sepa-credit-transfers": true,
+      "payments_pain.001-target-2-payments": true,
+      "payments_pain.001-cross-border-credit-transfers": true,
+      "bulk-payments_sepa-credit-transfers": true,
+      "bulk-payments_instant-sepa-credit-transfers": true,
+      "bulk-payments_target-2-payments": true,
+      "bulk-payments_cross-border-credit-transfers": true,
+      "bulk-payments_pain.001-sepa-credit-transfers": true,
+      "bulk-payments_pain.001-instant-sepa-credit-transfers": true,
+      "bulk-payments_pain.001-target-2-payments": true,
+      "bulk-payments_pain.001-cross-border-credit-transfers": true,
+      "periodic-payments_sepa-credit-transfers": true,
+      "periodic-payments_instant-sepa-credit-transfers": true,
+      "periodic-payments_target-2-payments": true,
+      "periodic-payments_cross-border-credit-transfers": true,
+      "periodic-payments_pain.001-sepa-credit-transfers": true,
+      "periodic-payments_pain.001-instant-sepa-credit-transfers": true,
+      "periodic-payments_pain.001-target-2-payments": true,
+      "periodic-payments_pain.001-cross-border-credit-transfers": true
+    },
+    "description": "Support of payment cancellation per payment product"
+  },
+  "IO44": {
+    "options": {
+      "xml": true,
+      "json": true
+    },
+    "description": "Supported formats of payment status response bodies for XML-based payments"
+  },
+  "IO45": {
+    "options": {
+      "batch_booking": true,
+      "realtime_booking": true
+    },
+    "description": "Processing of regular (not instant) payments"
+  },
+  "IO46": {
+    "options": {
+      "available": true,
+      "with_restrictions": true
+    },
+    "description": "Permission of requests for account data reading with PSU involvement and reference to a recurring consent."
+  },
+  "IO47": {
+    "options": {
+      "all": true,
+      "cumulated": true
+    },
+    "description": "Counting the frequency of AIS requests"
+  },
+  "IO48": {
+    "options": {
+      "available": true
+    },
+    "description": "Endpoint signing-baskets/{basketId} supported"
+  },
+  "IO49": {
+    "options": {
+      "available": true
+    },
+    "description": "Endpoint signing-baskets/{basketId}/status supported"
+  }
+}'
+WHERE configs.id = (SELECT aspsps.config_id
+                    FROM aspsps
+                    WHERE aspsps.aspsp_group_id =
+                          (SELECT aspsp_groups.id FROM aspsp_groups WHERE name ILIKE '%sparda%' LIMIT 1)
+                    LIMIT 1);

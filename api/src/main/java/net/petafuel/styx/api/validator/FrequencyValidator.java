60c17f8315c7fe6602b224aa5f23957e4e799de6
pF-luis <52947253+pf-luis@users.noreply.github.com>
2021-01-20 09:45:30 +0100

    FrequencyValidator wrong validation fixed

--- api/src/main/java/net/petafuel/styx/api/validator/FrequencyValidator.java
+++ api/src/main/java/net/petafuel/styx/api/validator/FrequencyValidator.java
@@ -8,7 +8,7 @@ import javax.validation.ConstraintValidatorContext;
 public class FrequencyValidator implements ConstraintValidator<ValidateFrequency, String> {
     @Override
     public boolean isValid(String frequency, ConstraintValidatorContext constraintValidatorContext) {
-        return (Frequency.getValue(frequency) == null);
+        return (Frequency.getValue(frequency) != null);
     }
 }
 

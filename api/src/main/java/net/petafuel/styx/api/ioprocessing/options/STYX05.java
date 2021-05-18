2545b1c0406cbe5287643bd1a7fb182779205a10
pF-luis <52947253+pf-luis@users.noreply.github.com>
2021-05-18 14:48:34 +0200

    sync already added commit from sprint to master

--- api/src/main/java/net/petafuel/styx/api/ioprocessing/options/STYX05.java
+++ api/src/main/java/net/petafuel/styx/api/ioprocessing/options/STYX05.java
@@ -35,7 +35,7 @@ public class STYX05 extends ApplicableImplementerOption {
             Boolean optionRequired = ioParser.getOption(IO, IOParser.Option.REQUIRED);
             if (optionRequired != null && optionRequired) {
                 StrongAuthenticatableResource response = (StrongAuthenticatableResource) xs2AResponse;
-                String authorizationLink = response.getLinks().getScaOAuth().getUrl();
+                String authorizationLink = response.getLinks().getScaRedirect().getUrl();
                 OAuthSession oAuthSession = OAuthSession.start(response.getxRequestId());
                 oAuthSession.setScope(SCAHandler.getQueryParameterValue(authorizationLink, "scope"));
                 oAuthSession.setAuthorizationEndpoint(getBaseUrl(authorizationLink));
@@ -51,7 +51,7 @@ public class STYX05 extends ApplicableImplementerOption {
                 String generatedCodeChallenge = OAuthService.getCodeChallengeFromState(oAuthSession.getState());
                 String codeChallengePlaceholder = SCAHandler.getQueryParameterValue(authorizationLink, "code_challenge");
                 authorizationLink = authorizationLink.replace(codeChallengePlaceholder, generatedCodeChallenge) + "&state=" + oAuthSession.getState();
-                response.getLinks().setScaOAuth(new Links.Href(authorizationLink, LinkType.SCA_OAUTH));
+                response.getLinks().setScaRedirect(new Links.Href(authorizationLink, LinkType.SCA_REDIRECT));
                 return true;
             }
         }

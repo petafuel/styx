aa4e6bbd195e7aca6699f8da337821c2ef5b59b2
pF-anton <54109482+pF-anton@users.noreply.github.com>
2021-11-26 13:05:10 +0100

    Tools-10700 ING Custom link responses

--- core/src/main/java/net/petafuel/styx/core/xs2a/entities/serializers/HrefTypeDeserializer.java
+++ core/src/main/java/net/petafuel/styx/core/xs2a/entities/serializers/HrefTypeDeserializer.java
@@ -2,6 +2,8 @@ package net.petafuel.styx.core.xs2a.entities.serializers;
 
 import net.petafuel.styx.core.xs2a.entities.LinkType;
 import net.petafuel.styx.core.xs2a.entities.Links;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.yasson.internal.JsonbParser;
 
 import javax.json.JsonObject;
@@ -14,22 +16,27 @@ import javax.json.stream.JsonParser;
 import java.lang.reflect.Type;
 
 public class HrefTypeDeserializer implements JsonbDeserializer<Links.Href> {
+    private static final Logger LOG = LogManager.getLogger(HrefTypeDeserializer.class);
 
     @Override
     public Links.Href deserialize(JsonParser parser, DeserializationContext ctx, Type rtType) {
-
-        Jsonb jsonb = JsonbBuilder.create();
         JsonValue parsedValue = parser.getValue();
+        Links.Href href = new Links.Href();
 
         if (parsedValue instanceof JsonObject) {
-            return jsonb.fromJson(parsedValue.toString(), Links.Href.class);
+            try (Jsonb jsonb = JsonbBuilder.create()) {
+                href = jsonb.fromJson(parsedValue.toString(), Links.Href.class);
+            } catch (Exception e) {
+                LOG.error("Could not initiate jsonb instance. ErrorMessage={}", e.getMessage());
+            }
         } else {
             LinkType linkType = null;
             if (parser instanceof JsonbParser) {
                 JsonbParser jsonbParser = (JsonbParser) parser;
                 linkType = LinkType.getByString(jsonbParser.getCurrentLevel().getLastKeyName());
             }
-            return new Links.Href(parsedValue.toString().replace("\"", ""), linkType);
+            href = new Links.Href(parsedValue.toString().replace("\"", ""), linkType);
         }
+        return href;
     }
 }

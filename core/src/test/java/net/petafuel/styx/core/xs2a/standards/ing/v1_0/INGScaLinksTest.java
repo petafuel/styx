8e1f164e037a0ba67434c1394697072325ca9a09
pF-anton <54109482+pF-anton@users.noreply.github.com>
2021-11-30 16:16:36 +0100

    Tools-10700 ING Custom link responses

--- core/src/test/java/net/petafuel/styx/core/xs2a/standards/ing/v1_0/INGScaLinksTest.java
+++ core/src/test/java/net/petafuel/styx/core/xs2a/standards/ing/v1_0/INGScaLinksTest.java
@@ -1,11 +1,11 @@
 package net.petafuel.styx.core.xs2a.standards.ing.v1_0;
 
 
-import jakarta.json.bind.Jsonb;
-import jakarta.json.bind.JsonbBuilder;
 import net.petafuel.styx.core.xs2a.entities.InitiatedPayment;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 
 
 class INGScaLinksTest {
@@ -13,37 +13,31 @@ class INGScaLinksTest {
     @Test
     void testHrefSerializer() {
         String body = "{\n" +
-                "    \"links\": {\n" +
-                "        \"scaRedirect\": {\n" +
-                "            \"href\": \"https://myaccount.ing.com/payment-initiation/c985e35c-61a3-4b32-85fa-e7b39228c361/XX\"\n" +
-                "        },\n" +
-                "        \"self\": {\n" +
-                "            \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/c985e35c-61a3-4b32-85fa-e7b39228c361\"\n" +
-                "        },\n" +
-                "        \"status\": {\n" +
-                "            \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/c985e35c-61a3-4b32-85fa-e7b39228c361/status\"\n" +
-                "        }\n" +
-                "    },\n" +
-                "    \"paymentId\": \"c985e35c-61a3-4b32-85fa-e7b39228c361\",\n" +
-                "    \"transactionStatus\": \"RCVD\"\n" +
-                "}";
+                "  \"_links\": {\n" +
+                "    \"scaRedirect\": \"https://myaccount.ing.com/payment-initiation/023feba0-ce9b-4f6b-be09-0bff281ccb55/XX\",\n" +
+                "    \"self\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/023feba0-ce9b-4f6b-be09-0bff281ccb55\",\n" +
+                "    \"status\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/023feba0-ce9b-4f6b-be09-0bff281ccb55/status\"\n" +
+                "  },\n" +
+                "  \"paymentId\": \"023feba0-ce9b-4f6b-be09-0bff281ccb55\",\n" +
+                "  \"transactionStatus\": \"RCVD\"\n" +
+                "}\n";
         String bodyWithHref = "{\n" +
-                "    \"links\": {\n" +
-                "        \"scaRedirect\": {\n" +
-                "            \"href\": \"https://idp-mock.sparda-n.de.schulung.sparda.de/oauth2/authorize?bic=TEST7999&client_id=PSDDE-BAFIN-125314&redirect_uri=https://preprod-styx.paycenter.de:8450/v1/callbacks/payment/ok/289de4c3-76fd-4b36-8cfa-d95ea955693d&response_type=code&scope=PIS:tx-fc639818c9a9b2ee2963e8695ae340da567b8116481479ad7c56956156c1e855&code_challenge_method=S256&code_challenge=5oLRnVEZKP0n9mh6v5FDqQZ04kEuhVa2tN2egP3YlD4&state=52322620-33d5-42f4-a6d9-13e73fbce5e9\"\n" +
-                "        },\n" +
-                "        \"scaStatus\": {\n" +
-                "            \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/s0wPCzRdC2il1QR_lqMI5X7Z6X9GRdeytUZ5cUaaOiCTs1rXbVNS4zgWoUl2-cHCjOC-zAu3SObPSQr46D3RMME8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q/authorisations/be0cf6a8-29ad-4fac-acd3-7486bb940529\"\n" +
-                "        },\n" +
-                "        \"self\": {\n" +
-                "            \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/s0wPCzRdC2il1QR_lqMI5X7Z6X9GRdeytUZ5cUaaOiCTs1rXbVNS4zgWoUl2-cHCjOC-zAu3SObPSQr46D3RMME8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q\"\n" +
-                "        },\n" +
-                "        \"status\": {\n" +
-                "            \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/s0wPCzRdC2il1QR_lqMI5X7Z6X9GRdeytUZ5cUaaOiCTs1rXbVNS4zgWoUl2-cHCjOC-zAu3SObPSQr46D3RMME8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q/status\"\n" +
-                "        }\n" +
+                "  \"_links\": {\n" +
+                "    \"scaRedirect\": {\n" +
+                "      \"href\": \"https://idp-mock.sparda-n.de.schulung.sparda.de/oauth2/authorize?bic=TEST7999&client_id=PSDDE-BAFIN-125314&redirect_uri=https://preprod-styx.paycenter.de:8450/v1/callbacks/payment/ok/79b19aae-2a6e-4afe-889d-dfe32190fad8&response_type=code&scope=PIS:tx-d6e3b38f7456cd1f2a7088c5588a603a282c5486001c8e50076641256efef369&code_challenge_method=S256&code_challenge=0Tk_vTrN56Lo9tngmcrwqnJDlDY5V3v4jnqfM2b3_bY&state=a6e76223-06b7-490c-a4fa-6cbef4b85c36\"\n" +
                 "    },\n" +
-                "    \"paymentId\": \"s0wPCzRdC2il1QR_lqMI5X7Z6X9GRdeytUZ5cUaaOiCTs1rXbVNS4zgWoUl2-cHCjOC-zAu3SObPSQr46D3RMME8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q\",\n" +
-                "    \"transactionStatus\": \"RCVD\"\n" +
+                "    \"scaStatus\": {\n" +
+                "      \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/35ngApqKH9d5icGeUcMSlqT5DA9svEL8n0HXL8sqkcBv_IV4muLvF3J7pWTqrZzQh623L3yTr2un-vqsP38W38E8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q/authorisations/a94c348e-0bd1-416a-a5c2-8dbf15fbdbfd\"\n" +
+                "    },\n" +
+                "    \"self\": {\n" +
+                "      \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/35ngApqKH9d5icGeUcMSlqT5DA9svEL8n0HXL8sqkcBv_IV4muLvF3J7pWTqrZzQh623L3yTr2un-vqsP38W38E8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q\"\n" +
+                "    },\n" +
+                "    \"status\": {\n" +
+                "      \"href\": \"https://preprod-styx.paycenter.de:8452/v1/payments/sepa-credit-transfers/35ngApqKH9d5icGeUcMSlqT5DA9svEL8n0HXL8sqkcBv_IV4muLvF3J7pWTqrZzQh623L3yTr2un-vqsP38W38E8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q/status\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"paymentId\": \"35ngApqKH9d5icGeUcMSlqT5DA9svEL8n0HXL8sqkcBv_IV4muLvF3J7pWTqrZzQh623L3yTr2un-vqsP38W38E8ZcgkupoAptjViDqg52c=_=_psGLvQpt9Q\",\n" +
+                "  \"transactionStatus\": \"RCVD\"\n" +
                 "}";
 
         Jsonb jsonb = JsonbBuilder.create();
@@ -51,8 +45,10 @@ class INGScaLinksTest {
         InitiatedPayment payment2 = jsonb.fromJson(bodyWithHref, InitiatedPayment.class);
         Assertions.assertNotNull(payment1.getPaymentId());
         Assertions.assertNotNull(payment1.getLinks().getScaRedirect());
+        Assertions.assertEquals("https://myaccount.ing.com/payment-initiation/023feba0-ce9b-4f6b-be09-0bff281ccb55/XX", payment1.getLinks().getScaRedirect().getUrl());
         Assertions.assertNotNull(payment2.getPaymentId());
         Assertions.assertNotNull(payment2.getLinks().getScaRedirect());
+        Assertions.assertEquals("https://idp-mock.sparda-n.de.schulung.sparda.de/oauth2/authorize?bic=TEST7999&client_id=PSDDE-BAFIN-125314&redirect_uri=https://preprod-styx.paycenter.de:8450/v1/callbacks/payment/ok/79b19aae-2a6e-4afe-889d-dfe32190fad8&response_type=code&scope=PIS:tx-d6e3b38f7456cd1f2a7088c5588a603a282c5486001c8e50076641256efef369&code_challenge_method=S256&code_challenge=0Tk_vTrN56Lo9tngmcrwqnJDlDY5V3v4jnqfM2b3_bY&state=a6e76223-06b7-490c-a4fa-6cbef4b85c36", payment2.getLinks().getScaRedirect().getUrl());
 
     }
 }
